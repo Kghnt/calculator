c8f22d37fd42d6f36c832384249ed414111f5615
Kghnt <154658735+Kghnt@users.noreply.github.com>
2024-04-13 12:56:27 +0800

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -2,35 +2,56 @@
 #include <stdio.h>
 #include "calculator.h" // 包含头文件
 #include "Tri_func.h"
-
+#include "test.h"
 void Instructions();
 int global_flag;//用于判断error输入
 
 int main() 
 {
-    char choice = 'y';
-    Instructions();
-    while (choice == 'y' || choice == 'Y') 
-    { 
-        global_flag = 0;
-        printf("请输入表达式（支持四则运算，可包含括号）：\n");
-        double result = expression();
-        if (global_flag == 0) 
-        {
-            printf("结果为： %.8g\n\n", result);//输出8位有效数字
-        }
-        else if (global_flag == 1)
-        {
-            printf("错误：无效的输入\n\n");
-            while (getchar() != '\n');
-        }
-        printf("是否继续计算？若继续请输入y，输入其余键退出: ");
-        //getchar();//
-        scanf(" %c", &choice);  //空格吸收上一次输入的换行符
-        while (getchar() != '\n'); // 清空输入缓冲区
+    char mode_choice;
+    printf("请选择测试或计算\n");
+    printf("A：测试，B：计算\n");
+    scanf("%c", &mode_choice);
+    getchar();
+    if (mode_choice == 'A' || mode_choice == 'a') 
+    {
+        testTrigFunctions();
     }
-    printf("谢谢使用！\n");
+    else if (mode_choice == 'B' || mode_choice == 'b') 
+    {
+        char choice = 'y';
+        Instructions();
+        while (choice == 'y' || choice == 'Y')
+        {
+            global_flag = 0;
+            printf("请输入表达式（支持四则运算，可包含括号）：\n");
+            double result = expression();
+            if (global_flag == 0)
+            {
+                printf("结果为： %.8g\n\n", result);//输出8位有效数字
+            }
+            else if (global_flag == 1)
+            {
+                printf("错误：无效的输入\n\n");
+                while (getchar() != '\n');
+            }
+            printf("是否继续计算？若继续请输入y，输入其余键退出: ");
+            //getchar();//
+            scanf(" %c", &choice);  //空格吸收上一次输入的换行符
+            while (getchar() != '\n'); // 清空输入缓冲区
+        }
+        printf("谢谢使用！\n");
+        
+    }
+    else 
+    {
+        printf("错误：无效的输入\n");
+        return 1; // 报错并退出主函数
+    }
+
     return 0;
+
+    
 }
 
 void Instructions()
